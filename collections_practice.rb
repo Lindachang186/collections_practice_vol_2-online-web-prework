ec68d6f123c7f3756cbb1202871f8c16ecaac4b2
Linda Chang <lindachang186@gmail.com>
2018-10-26 16:48:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,39 @@
 # your code goes here
+def begins_with_r(array)
+ var = true
+ array.each do |item|
+     var = false if item[0] != "r"
+ end
+  var
+ end
+
+
+def contain_a(array)
+  a = []
+  array.each do |string|
+    a << string if string.include?("a")
+  end
+  return a
+end
+
+def first_wa(array)
+  i = 0
+  while i < array.size
+    if array[i].to_s.start_with?("wa") == true
+      return array[i]
+    else
+    i +=1
+end
+end
+end
+
+def remove_non_strings(array)
+  array.delete_if {|a| a.class != String}
+return array
+end
+
+def count_elements(array)
+  array.each do |item|
+  item.count()
+end
+end
